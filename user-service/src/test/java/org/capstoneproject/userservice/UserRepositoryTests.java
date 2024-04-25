4617fa6ff0e74d266c5f6ddb5126c73101f311a2
Pukar <csepukar@gmail.com>
2024-04-25 12:42:40 +1000

    Add create user test case

--- user-service/src/test/java/org/capstoneproject/userservice/UserRepositoryTests.java
+++ user-service/src/test/java/org/capstoneproject/userservice/UserRepositoryTests.java
@@ -1,5 +1,6 @@
 package org.capstoneproject.userservice;
 
+import org.capstoneproject.userservice.repository.UserRepository;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
@@ -9,5 +10,28 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
 public class UserRepositoryTests {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private UserRepository repo;
+
+    @Test
+    public void testCreateUser() {
+        User user = new User();
+        user.setEmail("pukar@gmail.com");
+        user.setPassword("password");
+        user.setFirstName("Pukar");
+        user.setLastName("Sharma");
+
+        User savedUser = repo.save(user);
+        User existUser = entityManager.find(User.class, savedUser.getId());
+        assertThat(user.getEmail()).isEqualTo(existUser.getEmail());
+    }
 }
