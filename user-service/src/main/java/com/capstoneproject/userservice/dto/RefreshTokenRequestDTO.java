17c56780f6a23f175e6753d626173e476deb4480
Pukar <csepukar@gmail.com>
2024-04-29 22:28:17 +1000

    Add other services for user

---
+++ user-service/src/main/java/com/capstoneproject/userservice/dto/RefreshTokenRequestDTO.java
@@ -0,0 +1,12 @@
+package com.capstoneproject.userservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RefreshTokenRequestDTO {
+    private String token;
+}
--- user-service/src/main/java/com/capstoneproject/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/capstoneproject/userservice/repository/UserRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     public User findByUsername(String username);
-    // User findFirstById(Long id);
+    User findFirstById(Long id);
 }
