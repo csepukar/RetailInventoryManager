2d234c5a41d1084d8809372167567e0ee09ccb97
Kailashkaji Niraula <aavinavkaji@gmail.com>
2024-05-17 04:08:56 +1000

    refresh token fixed

--- user-service/src/main/java/com/capstoneproject/userservice/repository/RefreshTokenRepository.java
+++ user-service/src/main/java/com/capstoneproject/userservice/repository/RefreshTokenRepository.java
@@ -3,9 +3,9 @@ package com.capstoneproject.userservice.repository;
 //import com.capstoneproject.userservice.helper.RefreshableCRUDRepository;
 import com.capstoneproject.userservice.model.RefreshToken;
 import com.capstoneproject.userservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.Optional;
 
 @Repository
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Integer> {
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Integer> {
 
     Optional<RefreshToken> findByToken(String token);
     void deleteAll();
--- user-service/src/main/java/com/capstoneproject/userservice/service/RefreshTokenService.java
+++ user-service/src/main/java/com/capstoneproject/userservice/service/RefreshTokenService.java
@@ -4,13 +4,17 @@ import com.capstoneproject.userservice.model.RefreshToken;
 import com.capstoneproject.userservice.model.User;
 import com.capstoneproject.userservice.repository.RefreshTokenRepository;
 import com.capstoneproject.userservice.repository.UserRepository;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.security.Key;
 import java.time.Instant;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 public class RefreshTokenService {
@@ -31,12 +35,25 @@ public class RefreshTokenService {
         // Continue rest of the process
         RefreshToken refreshToken = RefreshToken.builder()
                 .userInfo(userRepository.findByUsername(username))
-                .token(UUID.randomUUID().toString())
+                .token(createToken(new HashMap<>(), username))
                 .expiryDate(Instant.now().plusMillis(600000)) // set expiry of refresh token to 10 minutes - you can configure it application.properties file
                 .build();
         return refreshTokenRepository.save(refreshToken);
     }
+    private String createToken(Map<String, Object> claims, String username) {
 
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*10))
+                .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode("357638792F423F4428472B4B6250655368566D597133743677397A2443264629");
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
     public Optional<RefreshToken> findByToken(String token){
         return refreshTokenRepository.findByToken(token);
     }
